93eacd5ab824dfacaa5669a43658a9c6db3c7bb6
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-03-13 12:41:24 +0530

    Update help.js

--- Commands/Core/help.js
+++ Commands/Core/help.js
@@ -56,6 +56,15 @@ module.exports = {
 
 const now = new Date();
        const hour = now.getHours();
+      let greeting;
+      
+      if (hour >= 0 && hour < 12) {
+        greeting = "Ohayou gozaimasu"; //good morning
+      } else if (hour >= 12 && hour < 18) {
+        greeting = "Konnichiwa"; //good afternoon
+      } else {
+        greeting = "Konbanwa"; //good evening
+      }
       let textHelpMenu = `Konnichiwa *${pushName}* Senpai,
 
 I am *${botName}*, a bot Edited by *Team Celestic*.
