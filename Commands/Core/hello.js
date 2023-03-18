6e8ab27bda886bc417b0d9c61ba0f8c40e5dc23f
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-03-19 01:46:04 +0530

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "hi",
-    alias: ["hello","alive"],
+    alias: ["hello","hey"," konnichiwa"],
     desc: "Say hello to bot.",
     react: "🧣",
     category: "Core",
@@ -13,6 +13,6 @@ module.exports = {
         return time = `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
         };
         const uptime = () => formatTime(process.uptime());
-        await Miku.sendMessage(m.from,{image:{url:botImage1},caption:`\nKonichiwa *${pushName}* senpai, I am *${botName}* bot. type *${prefix}help* to get my full command list.\n\n_🧩 Server Uptime:_ *${uptime()}*\n_🎀 Status:_ *Operational*\n`},{quoted:m})
+        await Miku.sendMessage(m.from,{image:{url:botImage1},caption:`\nKonnichiwa *${pushName}* senpai, I am *${botName}* bot. type *${prefix}help* to get my full command list.\n\n_🧩 Server Uptime:_ *${uptime()}*\n_🎀 Status:_ *Operational*\n`},{quoted:m})
     }
 }
