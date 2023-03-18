3a1e3393578d99365160476075dd4ccc237eed2d
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-03-19 01:46:25 +0530

    Add files via upload

---
+++ Commands/Core/alive.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "alive",
+    alias: ["bs"],
+    desc: "Bot status.",
+    react: "🧣",
+    category: "Core",
+        start: async(Miku, m,{pushName,prefix}) => {
+        const pad = (s) => (s < 10 ? "0" : "") + s;
+        const formatTime = (seconds) => {
+        const hours = Math.floor(seconds / (60 * 60));
+        const minutes = Math.floor((seconds % (60 * 60)) / 60);
+        const secs = Math.floor(seconds % 60);
+        return time = `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
+        };
+        const uptime = () => formatTime(process.uptime());
+        await Miku.sendMessage(m.from,{image:{url:botImage2},caption:`\nKonnichiwa *${pushName}* senpai, I am *${botName}* bot V6.0. \n\n_💝I am Alive.\n\n_🧩 Server Uptime:_ *${uptime()}*\n_🎀 Status:_ *Operational*\n`},{quoted:m})
+    }
+}
