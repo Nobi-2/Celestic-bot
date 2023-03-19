134de3719709fbe560849580239fe7669f53d1a3
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 15:14:25 +0530

    Update straightcheck.js

--- Commands/Fun/straightcheck.js
+++ Commands/Fun/straightcheck.js
@@ -3,7 +3,7 @@ module.exports = {
   alias: ["straightcheck"],
   desc: "check",
   cool: 3,
-  react: "😆",
+  react: "🏃",
   category: "Fun",
   start: async (
     Miku,
@@ -24,7 +24,7 @@ const dey = shibam(1, 100)
     
     let Mikutext = `Straight Check Of : @${
       mentionedUser.split("@")[0]
-    }\n\nAnswer : *${dey}%*🤣`;
+    }\n\nAnswer : *${dey}%*😹`;
 
     Miku.sendMessage(
       m.from,
