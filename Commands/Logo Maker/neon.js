e6834a836ec528a5ecd4595a6741d8b973701be9
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 17:32:12 +0530

    Update neon.js

--- Commands/Logo Maker/neon.js
+++ Commands/Logo Maker/neon.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "neon",
     alias: ["neonstyle"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🎇",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}neon Atlas Bot*`);
