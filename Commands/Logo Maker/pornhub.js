3cfddd22d6e644fce42d2f67df3c3b3aafc31ec2
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 17:35:50 +0530

    Update pornhub.js

--- Commands/Logo Maker/pornhub.js
+++ Commands/Logo Maker/pornhub.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "pornhub",
     alias: ["ph"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "💦",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text.includes("|")) return m.reply(`Example: *${prefix}pornhub Atlas Bot|Team Atlas*`);
