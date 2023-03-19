3099188061d2805819ae06f9be5ea36dcef2c8e0
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 15:10:59 +0530

    Update hornycheck.js

--- Commands/Fun/hornycheck.js
+++ Commands/Fun/hornycheck.js
@@ -3,7 +3,7 @@ module.exports = {
   alias: ["horchek"],
   desc: "check",
   cool: 3,
-  react: "😆",
+  react: "🤤",
   category: "Fun",
   start: async (
     Miku,
@@ -24,7 +24,7 @@ module.exports = {
 const dey = shibam(1, 100)
     let Mikutext = `Horny Check Of : @${
       mentionedUser.split("@")[0]
-    }\n\nAnswer : *${dey}%*🤣`;
+    }\n\nAnswer : *${dey}%*🤤`;
 
     Miku.sendMessage(
       m.from,
