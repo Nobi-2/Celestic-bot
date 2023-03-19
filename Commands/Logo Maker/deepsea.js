c42e8b12fe9966dcb32548ab64c372849ede7509
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 17:27:05 +0530

    Update deepsea.js

--- Commands/Logo Maker/deepsea.js
+++ Commands/Logo Maker/deepsea.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "deepsea",
     alias: ["deepseastyle"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🐈‍⬛",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}deepsea Atlas Bot*`);
