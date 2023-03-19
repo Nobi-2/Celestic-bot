3005d7ec7aca5b048e6e894389510ce3821f2e50
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 17:38:01 +0530

    Update wolf.js

--- Commands/Logo Maker/wolf.js
+++ Commands/Logo Maker/wolf.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "wolf",
     alias: ["wolfstyle","wolflogo"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🐺",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text.includes("|")) return m.reply(`Example: *${prefix}wolf Atlas Bot | Team Atlas*`);
