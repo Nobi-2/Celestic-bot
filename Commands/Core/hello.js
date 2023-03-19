b5310d0091f09fdbe3b9e63e9f62792e35801d84
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 20:08:16 +0530

    Update hello.js

--- Commands/Core/hello.js
+++ Commands/Core/hello.js
@@ -13,6 +13,6 @@ module.exports = {
         return time = `${pad(hours)}:${pad(minutes)}:${pad(secs)}`;
         };
         const uptime = () => formatTime(process.uptime());
-        await Miku.sendMessage(m.from,{image:{url:botImage1},caption:`\nKonnichiwa *${pushName}* senpai, I am *${botName}* bot. type *${prefix}help* to get my full command list.\n\n_🌸 Server Uptime:_ *${uptime()}*\n_💐 Status:_ *Operational*\n`},{quoted:m})
+        await Miku.sendMessage(m.from,{image:{url:botImage1},caption:`\nKonnichiwa *${pushName}* senpai, I am *${botName}* bot. type *${prefix}help* to get my full command list.\n\n_🌸 Server Uptime:_ *${uptime()}*\n_❄️ Status:_ *Operational*\n`},{quoted:m})
     }
 }
