b4be64572f878eb661594b91934a2f7967121666
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 17:30:35 +0530

    Update joker.js

--- Commands/Logo Maker/joker.js
+++ Commands/Logo Maker/joker.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "joker",
     alias: ["joker"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🤡",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text) return m.reply(`Example: *${prefix}joker Atlas Bot*`);
