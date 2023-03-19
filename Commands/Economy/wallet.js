d50114a2e49f668619f9ce2673e4d41bddef1d3f
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 14:22:58 +0530

    Update wallet.js

--- Commands/Economy/wallet.js
+++ Commands/Economy/wallet.js
@@ -11,7 +11,7 @@ module.exports = {
     desc: "Shows Wallet.",
     alias: ["wallet"], 
     category: "Economy",  
-    react: "💲", 
+    react: "👝", 
     start: async ( 
         Miku, 
       m, 
