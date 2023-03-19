a71deccbdda421730343bc42a2e1e00abc4b81ce
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 19:08:39 +0530

    Update google.js

--- Commands/Search/google.js
+++ Commands/Search/google.js
@@ -6,7 +6,7 @@ module.exports = {
     desc: "Search something in google",
     category: "Search",
     usage: `google <search term>`,
-    react: "🍁",
+    react: "⚙️",
     start: async (Miku, m, { text, prefix, args }) => {
       if (!args[0])
         return Miku.sendMessage(
@@ -22,10 +22,10 @@ module.exports = {
             console.log(error);
             });
 
-        let resText = `  *『  ⚡️ Google Search Engine ⚡️  』*\n\n\n_🔍 Search Term:_ *${googlesearchTerm}*\n\n\n`
+        let resText = `  *『  🌀 Google Search Engine 🌀  』*\n\n\n_🔍 Search Term:_ *${googlesearchTerm}*\n\n\n`
 
         for(num=0; num<10; num++){
-            resText += `_📍 Result:_ *${num+1}*\n\n_🎀 Title:_ *${googleSearch[num].title}*\n\n_🔶 Description:_ *${googleSearch[num].snippet}*\n\n_🔷 Link:_ *${googleSearch[num].link}*\n\n\n`;
+            resText += `_❄️ Result:_ *${num+1}*\n\n_📨 Title:_ *${googleSearch[num].title}*\n\n_🔶 Description:_ *${googleSearch[num].snippet}*\n\n_🔷 Link:_ *${googleSearch[num].link}*\n\n\n`;
         }
 
       await Miku.sendMessage(
