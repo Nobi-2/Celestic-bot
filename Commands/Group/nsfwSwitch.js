3e3ff4644296f94bb08d8358dfaf6cd1102db9a8
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 13:22:05 +0530

    Update nsfwSwitch.js

--- Commands/Group/nsfwSwitch.js
+++ Commands/Group/nsfwSwitch.js
@@ -6,7 +6,7 @@ module.exports = {
     desc: "Enable or disable NSFW commands in a group",
     category: "Group",
     usage: "nsfw [on/off]",
-    react: "🎀",
+    react: "🤤",
     start: async (
       Miku,
       m,
