21ed80b7ab2cfb8e9d7391f4c3097b297bd57571
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-03-19 21:31:35 +0530

    Update wall.js

--- Commands/Logo Maker/wall.js
+++ Commands/Logo Maker/wall.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🧱",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}wall Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}wall Celestic Bot*`);
         maker.textpro("https://textpro.me/break-wall-text-effect-871.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
