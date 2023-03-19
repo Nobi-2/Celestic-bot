e472f920ffc76c4817473361f263ccb0f1e3d6e0
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-19 15:47:54 +0530

    Update bear.js

--- Commands/Logo Maker/bear.js
+++ Commands/Logo Maker/bear.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "bear",
     alias: ["bearstyle","bearlogo"],
     desc: "Make text logo.",
-    react: "🍁",
+    react: "🐻",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
         if(!text.includes("|")) return m.reply(`Example: *${prefix}bear Atlas Bot | Team Atlas*`);
