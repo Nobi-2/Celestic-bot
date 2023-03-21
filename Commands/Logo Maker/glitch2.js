02adbde254bf8827921135ff96d9597b25caa6d3
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:58:48 +0530

    Update glitch2.js

--- Commands/Logo Maker/glitch2.js
+++ Commands/Logo Maker/glitch2.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "☄️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text.includes("|")) return m.reply(`Example: *${prefix}glitch2 Atlas Bot | Team Atlas*`);
+        if(!text.includes("|")) return m.reply(`Example: *${prefix}glitch2 celestic Bot | Nobi*`);
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/create-a-glitch-text-effect-online-free-1026.html", [
