7723cc8c27df04844a9eda82f757ede822e106b6
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 19:29:47 +0530

    Update anime.js

--- Commands/Search/anime.js
+++ Commands/Search/anime.js
@@ -7,7 +7,7 @@ module.exports = {
   desc: "To get an anime search result",
   category: "Search",
   usage: `anime <search term>`,
-  react: "🍁",
+  react: "🦄",
   start: async (Miku, m, { text, prefix, args }) => {
     if (!args[0])
       return Miku.sendMessage(
@@ -20,19 +20,19 @@ module.exports = {
     let anime = await client.searchAnime(AnimesearchTerm);
 
     let result = anime.data[0];
-    let details = `       *『  Anime Search Engine  』*\n\n\n*🎀 Anime Title:* ${result.title}\n`;
-    details += `\n*🎋 Format:* ${result.type}\n`;
+    let details = `       *『  Anime Search Engine  』*\n\n\n*🌸 Anime Title:* ${result.title}\n`;
+    details += `\n*👀 Format:* ${result.type}\n`;
     details += `*📈 Status:* ${result.status
       .toUpperCase()
       .replace(/\_/g, " ")}\n`;
     details += `*🍥 Total episodes:* ${result.episodes}\n`;
-    details += `*🎈 Duration:* ${result.duration}\n`;
-    details += `*🧧 Genres:*\n`;
+    details += `*🦾 Duration:* ${result.duration}\n`;
+    details += `*🌹 Genres:*\n`;
     for (let i = 0; i < result.genres.length; i++) {
       details += `\t\t\t\t\t\t\t\t${result.genres[i].name}\n`;
     }
-    details += `\n*✨ Based on:* ${result.source.toUpperCase()}\n`;
-    details += `*📍 Studios:*\n`;
+    details += `\n*🥀 Based on:* ${result.source.toUpperCase()}\n`;
+    details += `*🍫 Studios:*\n`;
     for (let i = 0; i < result.studios.length; i++) {
       details += `\t\t\t\t\t\t\t\t${result.studios[i].name}\n`;
     }
@@ -43,7 +43,7 @@ module.exports = {
     details += `\n*🎐 Popularity:* ${result.popularity}\n`;
     details += `*🎏 Favorites:* ${result.favorites}\n`;
     details += `*🎇 Rating:* ${result.rating}\n`;
-    details += `*🏅 Rank:* ${result.rank}\n\n`;
+    details += `*🔰 Rank:* ${result.rank}\n\n`;
     details += `\n*🌐 URL:* ${result.url}\n\n`;
 
     await Miku.sendMessage(
