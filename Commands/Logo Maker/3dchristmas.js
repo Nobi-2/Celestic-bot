abf1c48080dfd459ac05c420b99b828fe615cf47
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:33:52 +0530

    Update 3dchristmas.js

--- Commands/Logo Maker/3dchristmas.js
+++ Commands/Logo Maker/3dchristmas.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🌲",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}3dchristmas Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}3dchristmas celestic Bot*`);
         maker.textpro("https://textpro.me/3d-christmas-text-effect-by-name-1055.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
