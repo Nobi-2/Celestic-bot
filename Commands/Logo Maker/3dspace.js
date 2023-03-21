a9c04c759231df75e9eb2223e18a095cf649a885
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:36:03 +0530

    Update 3dspace.js

--- Commands/Logo Maker/3dspace.js
+++ Commands/Logo Maker/3dspace.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🐈‍⬛",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text.includes("|")) return m.reply(`Example: *${prefix}3dspace celestic Bot | Team Atlas*`);
+        if(!text.includes("|")) return m.reply(`Example: *${prefix}3dspace celestic Bot | Team celestic*`);
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/create-space-3d-text-effect-online-985.html", [
