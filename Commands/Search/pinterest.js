4b10310aa366353658f347b39962d59fd5d92fa0
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 19:27:01 +0530

    Update pinterest.js

--- Commands/Search/pinterest.js
+++ Commands/Search/pinterest.js
@@ -11,7 +11,7 @@ module.exports = {
     if (!args[0])
       return Miku.sendMessage(
         m.from,
-        { text: `Please provide a Search Term !` },
+        { text: `Abe kuch to bata search kya karna he !` },
         { quoted: m }
       );
     var PinsearchTerm = args.join(" ");
