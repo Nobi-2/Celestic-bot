cb3311aadc69459ef26f10f33ea273212e95a131
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 17:19:09 +0530

    Update pencil.js

--- Commands/Logo Maker/pencil.js
+++ Commands/Logo Maker/pencil.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "✏️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}pencil Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}pencil celestic Bot*`);
         maker.textpro("https://textpro.me/create-a-sketch-text-effect-online-1044.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
