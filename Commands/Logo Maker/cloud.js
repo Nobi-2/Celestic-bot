0da6757cd116fd8f367e407a0d70a6ca982e79a4
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:56:24 +0530

    Update cloud.js

--- Commands/Logo Maker/cloud.js
+++ Commands/Logo Maker/cloud.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "☁️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}cloud Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}cloud NOBITA NOBI*`);
         maker.textpro("https://textpro.me/create-a-cloud-text-effect-on-the-sky-online-1004.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
