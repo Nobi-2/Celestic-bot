fc6338ed287e227f7599aa6b041fb0eb824e727e
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:51:04 +0530

    Update bear.js

--- Commands/Logo Maker/bear.js
+++ Commands/Logo Maker/bear.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🐻",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text.includes("|")) return m.reply(`Example: *${prefix}bear Atlas Bot | Team Atlas*`);
+        if(!text.includes("|")) return m.reply(`Example: *${prefix}bear celestic Bot | Team Atlas*`);
         teks1 = text.split("|")[0]
         teks2 = text.split("|")[1]
         maker.textpro("https://textpro.me/online-black-and-white-bear-mascot-logo-creation-1012.html", [
