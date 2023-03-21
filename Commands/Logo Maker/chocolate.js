ab2a0fb7f82727b2e328b16447c787af084462e4
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:53:45 +0530

    Update chocolate.js

--- Commands/Logo Maker/chocolate.js
+++ Commands/Logo Maker/chocolate.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🍫",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}chocolate Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}chocolate nobita nobi*`);
         maker.textpro("https://textpro.me/chocolate-cake-text-effect-890.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
