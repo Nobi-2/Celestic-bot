f89a95d0161d494df91907359b22205ab433d91b
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 17:21:38 +0530

    Update thunder.js

--- Commands/Logo Maker/thunder.js
+++ Commands/Logo Maker/thunder.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "⚡",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}thunder Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}thunder celestic Bot*`);
         maker.textpro("https://textpro.me/online-thunder-text-effect-generator-1031.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
