01026c6d5955418ad9e83d682a30bb76182656df
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:51:59 +0530

    Update blood.js

--- Commands/Logo Maker/blood.js
+++ Commands/Logo Maker/blood.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "👺",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}blood Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}blood celestic Bot*`);
         maker.textpro("https://textpro.me/horror-blood-text-effect-online-883.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
