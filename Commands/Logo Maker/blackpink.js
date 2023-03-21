3d8a8ed94330d5326d9f56ecbcdd50d13a6d8944
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:51:30 +0530

    Update blackpink.js

--- Commands/Logo Maker/blackpink.js
+++ Commands/Logo Maker/blackpink.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🖤",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}blackpink Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}blackpink celestic Bot*`);
         maker.textpro("https://textpro.me/create-blackpink-logo-style-online-1001.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
