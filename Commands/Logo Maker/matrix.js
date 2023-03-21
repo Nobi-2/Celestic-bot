f7b7b57cdb5eba4a5cb0b730c091b0fc7b630f99
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 17:05:38 +0530

    Update matrix.js

--- Commands/Logo Maker/matrix.js
+++ Commands/Logo Maker/matrix.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "☄️",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}matrix Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}matrix celestic Bot*`);
         maker.textpro("https://textpro.me/matrix-style-text-effect-online-884.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
