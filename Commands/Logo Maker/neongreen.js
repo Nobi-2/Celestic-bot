1561ad2fd1000b28eb6a3f04beaa33a6c686a7c8
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 17:06:35 +0530

    Update neongreen.js

--- Commands/Logo Maker/neongreen.js
+++ Commands/Logo Maker/neongreen.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🍃",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}ngreen Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}ngreen celestic Bot*`);
         maker.textpro("https://textpro.me/green-neon-text-effect-874.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
