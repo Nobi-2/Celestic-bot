30c852964557d11f559dff30a67cdf215a6ff48a
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:54:30 +0530

    Update christmas.js

--- Commands/Logo Maker/christmas.js
+++ Commands/Logo Maker/christmas.js
@@ -2,12 +2,12 @@ const maker = require('mumaker')
 
 module.exports = {
     name: "christmas",
-    alias: ["christmastree"],
+    alias: ["christmastree","ctree"],
     desc: "Make text logo.",
     react: "🌲",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}christmas Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}christmas celestic Bot*`);
         maker.textpro("https://textpro.me/christmas-tree-text-effect-online-free-1057.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
