25b32a16b79e6bebc37367b8597534e11fc9f961
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 17:04:13 +0530

    Update joker.js

--- Commands/Logo Maker/joker.js
+++ Commands/Logo Maker/joker.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🤡",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}joker Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}joker celestic Bot*`);
         maker.textpro("https://textpro.me/create-logo-joker-online-934.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
