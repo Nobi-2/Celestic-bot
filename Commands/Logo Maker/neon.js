07cb9e1e843dbaaa23580b262821ca29c11d57ec
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 17:06:01 +0530

    Update neon.js

--- Commands/Logo Maker/neon.js
+++ Commands/Logo Maker/neon.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🎇",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}neon Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}neon celestic Bot*`);
         maker.textpro("https://textpro.me/neon-text-effect-online-879.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
