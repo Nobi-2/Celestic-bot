caa87cd2ee92871f11c150e2f1ac95c7c3e153af
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:52:25 +0530

    Update bokeh.js

--- Commands/Logo Maker/bokeh.js
+++ Commands/Logo Maker/bokeh.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🔨",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}bokeh Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}bokeh celestic Bot*`);
         maker.textpro("https://textpro.me/bokeh-text-effect-876.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
