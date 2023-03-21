e111629c5c352d740a091679f00fda5c7ae549f6
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:54:51 +0530

    Update circuit.js

--- Commands/Logo Maker/circuit.js
+++ Commands/Logo Maker/circuit.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "🐈‍⬛",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}circuit Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}circuit celestic Bot*`);
         maker.textpro("https://textpro.me/create-blue-circuit-style-text-effect-online-1043.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
