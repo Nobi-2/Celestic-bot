aee159896a986b774b295dc1c0398ac3dd1c815a
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 16:57:44 +0530

    Update dropwater.js

--- Commands/Logo Maker/dropwater.js
+++ Commands/Logo Maker/dropwater.js
@@ -7,7 +7,7 @@ module.exports = {
     react: "💦",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}dropwater Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}dropwater celestic Bot*`);
         maker.textpro("https://textpro.me/dropwater-text-effect-872.html", [
             `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
