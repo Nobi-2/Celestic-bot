5fa9f4a8749391601ebb400f9efaba376c716c6f
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-21 17:21:16 +0530

    Update sparklechristmas.js

--- Commands/Logo Maker/sparklechristmas.js
+++ Commands/Logo Maker/sparklechristmas.js
@@ -4,10 +4,10 @@ module.exports = {
     name: "sparklechristmas",
     alias: ["schristmas"],
     desc: "Make text logo.",
-    react: "🐈‍⬛",
+    react: "🌲",
     category: "Logo Maker",
     start: async(Miku, m,{pushName,prefix,text}) => {
-        if(!text) return m.reply(`Example: *${prefix}schristmas Atlas Bot*`);
+        if(!text) return m.reply(`Example: *${prefix}schristmas celestic Bot*`);
         maker.textpro("https://textpro.me/sparkles-merry-christmas-text-effect-1054.html", [
     `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
     .catch((err) => m.reply('An Error occued !'));
