0887fc367a812d2a5308c28118a2b1b11fdd1934
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-03-23 20:07:40 +0530

    Update bank.js

--- Commands/Economy/bank.js
+++ Commands/Economy/bank.js
@@ -13,9 +13,9 @@ module.exports = {
     start: async ( 
         Miku, 
       m, 
-      { text, prefix} 
+      { text, prefix, ecostatus} 
     ) => {
-      if (switchEconomy == "false") return m.reply(`This group is not Economy enabled!\n\nTo configure Economy mode, type:\n\n*${prefix}ecomenu*`);
+      if (ecostatus == "false") return m.reply(`This group is not Economy enabled!\n\nTo configure Economy mode, type:\n\n*${prefix}ecomenu*`);
         const pushname = m.pushName //|| 'NO name'
         const user = m.sender
     const cara = "cara"
