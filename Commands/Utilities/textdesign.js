e729bc93fac1211459248e2bc57279085c5b94b1
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-23 17:06:07 +0530

    Update textdesign.js

--- Commands/Utilities/textdesign.js
+++ Commands/Utilities/textdesign.js
@@ -11,7 +11,7 @@ module.exports = {
       if (!args[0])
         return Miku.sendMessage(
           m.from,
-          { text: `Please provide a Text !` },
+          { text: `Please provide a Text BAKA !` },
           { quoted: m }
         );
 
