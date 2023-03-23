9e3eee8a57fdd2958173868edce9b2f66524573b
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-23 16:05:29 +0530

    Update remove.js

--- Commands/Group/remove.js
+++ Commands/Group/remove.js
@@ -3,11 +3,11 @@ require("../../Core.js");
 
 module.exports = {
   name: "remove",
-  alias: ["rem"],
+  alias: ["rem","fuck","bhag","Nikal"],
   desc: "Remove a member from group",
   category: "Group",
   usage: "remove @user",
-  react: "👀",
+  react: "🗿",
   start: async (
     Miku,
     m,
