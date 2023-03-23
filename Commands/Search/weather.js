164123b275c3fd43e3688518d18d186347450aae
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-23 16:44:41 +0530

    Update weather.js

--- Commands/Search/weather.js
+++ Commands/Search/weather.js
@@ -11,7 +11,7 @@ module.exports = {
     if (!args[0])
       return Miku.sendMessage(
         m.from,
-        { text: `Please provide a location name !` },
+        { text: `Please provide a location name Baka !` },
         { quoted: m }
       );
     var WeatherSearchTerm = args.join(" ");
