d92b36d395825609fe73b08bc85b0d28a94a0ed4
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-23 16:38:54 +0530

    Update unban.js

--- Commands/Mods/unban.js
+++ Commands/Mods/unban.js
@@ -2,7 +2,7 @@ const { mku } = require("../../Database/dataschema.js");
 
 module.exports = {
   name: "unban",
-  alias: ["unban","reales"],
+  alias: ["unban","reales","forgive"],
   desc: "Unban a member",
   category: "core",
   usage: "unban @user",
