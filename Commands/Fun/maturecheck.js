94549eb3227c8722e0406e9991cddf4c43cc9212
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-23 16:00:27 +0530

    Update maturecheck.js

--- Commands/Fun/maturecheck.js
+++ Commands/Fun/maturecheck.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "maturecheck",
-  alias: ["maturechek"],
+  alias: ["maturechek","mature"],
   desc: "check",
   cool: 3,
   react: "😶",
@@ -29,7 +29,7 @@ const dey = shibam(1, 100)
     Miku.sendMessage(
       m.from,
       {
-        image: { url: botImage3 },
+        image: { url: botImage4 },
         caption: Mikutext,
         mentions: [mentionedUser],
       },
