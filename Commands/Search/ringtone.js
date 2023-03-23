2e56c407554381942d9fed7fc65b6de6a1611061
Nobi-2 <127091256+Nobi-2@users.noreply.github.com>
2023-03-23 16:45:04 +0530

    Update ringtone.js

--- Commands/Search/ringtone.js
+++ Commands/Search/ringtone.js
@@ -11,7 +11,7 @@ module.exports = {
     if (!args[0])
       return Miku.sendMessage(
         m.from,
-        { text: `Please provide a Search Term !` },
+        { text: `Please provide a Search Term Baka !` },
         { quoted: m }
       );
     var RTsearchTerm = args.join(" ");
