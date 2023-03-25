00767b9e5eb99bad6ac9a9c9ec2fe62ceed5ef87
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-03-26 01:44:39 +0530

    Update help.js

--- Commands/Core/help.js
+++ Commands/Core/help.js
@@ -55,12 +55,13 @@ module.exports = {
         const uptime = () => formatTime(process.uptime());
 
 const now = new Date();
-        const hour = now.getHours();
-       let greeting;
 
-        if (hour >= 00 && hour < 12) {
+        const hour = now.getHours();
+
+       let greeting;
+        if (hour >= 0 && hour < 12) {
           greeting = "Ohayou gozaimasu"; //good morning
-        } else if (hour >= 12 && hour < 16) {
+        } else if (hour >= 12 && hour < 18) {
           greeting = "Konnichiwa"; //good afternoon
         } else {
           greeting = "Konbanwa"; //good evening
