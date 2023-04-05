3340f2dabc0c5fca853633bcbd8d2ac2b662a18d
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-06 00:11:01 +0530

    Update V9.0

--- Commands/Economy/transfer.js
+++ Commands/Economy/transfer.js
@@ -10,6 +10,7 @@ module.exports = {
   category: "Economy",
   react: "💴",
   start: async (Miku, m, { text, prefix, mentionByTag, pushName }) => {
+    if (ECOstatus == "false") return m.reply(`This group is not Economy enabled!\n\nTo configure Economy mode, type:\n\n*${prefix}ecomenu*`);
     let value = text.trim().split(" ");
     if (value[0] === "") return m.reply(`Use ${prefix}transfer 100 @user`);
     if (!text && !m.quoted) {
