9ce274d01ecc9340cfa4f6da986e4bcd018b4fe5
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-05 13:57:43 +0530

    Update wallet.js

--- Commands/Economy/wallet.js
+++ Commands/Economy/wallet.js
@@ -17,41 +17,28 @@ module.exports = {
       m, 
       { text, prefix, isBotAdmin, isAdmin, mentionByTag, pushName, isCreator, ECOstatus} 
     ) => {
-      if (ECOstatus == "false") return m.reply(`This group is not Economy enabled!\n\nTo configure Economy mode, type:\n\n*${prefix}ecomenu*`);
-      if (m.quoted) {
-      var mentionedUser = m.quoted.sender;
-    }
-      if (m.quoted) {
-      var mentionedUser = mentionByTag[0];
-    } else {
-      var mentionedUser = m.sender;
-    }
-        let user = (await mentionedUser) || m.msg.contextInfo.participant; 
-         const cara = "cara"
-         const balance = await eco.balance(user, cara); //Returns wallet, bank, and bankCapacity. Also creates a USer if it doesn't exist.
-         let buttons = [
-            {
-              buttonId: `${prefix}deposit ${balance.wallet}`,
-              buttonText: { displayText: `Deposit All 💴` },
-              type: 1,
-            },
-            {
-                buttonId: `${prefix}Bank`,
-              buttonText: { displayText: "Bank 🏦" },
-              type: 1,
+    if (ECOstatus == "false") return m.reply(`This group is not Economy enabled!\n\nTo configure Economy mode, type:\n\n*${prefix}ecomenu*`);
+    if (m.quoted) {
+    var mentionedUser = m.quoted.sender;
+  }
+    if (m.quoted) {
+    var mentionedUser = mentionByTag[0];
+  } else {
+    var mentionedUser = m.sender;
+  }
+    let user = (await mentionedUser) || m.msg.contextInfo.participant; 
+    const cara = "cara"
+    const balance = await eco.balance(user, cara); //Returns wallet, bank, and bankCapacity. Also creates a USer if it doesn't exist.
+         
+    await Miku.sendMessage(
+      m.from,
+      {
+       image: fs.readFileSync("./Assets/Img/card.png"),
+       caption: `\n💳 *${m.pushName}'s Wallet:*\n\n_💴 ${balance.wallet}_`,
+      },
+      { quoted: m }
+    );
+  },
+};
 
-            },
-          ];
-          let buttonMessage = {
-            image: fs.readFileSync("./Assets/Img/card.png"), 
-            caption: `\n💳 *@${
-                  mentionedUser.split("@")[0]
-                 }'s Wallet:*\n\n_💴 ${balance.wallet}_`,
-            footer: `*${botName}*`,
-            buttons: buttons,
-            type: 4
-          };
         
-          await Miku.sendMessage(m.from, buttonMessage, { quoted: m });
-        }
-    }
