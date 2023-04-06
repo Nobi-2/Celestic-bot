b18c661a067de1f71583a63835b5477077f9b0ca
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-06 13:21:38 +0530

    Fix rm

--- Commands/Group/remove.js
+++ Commands/Group/remove.js
@@ -3,21 +3,6 @@ require("../../Core.js");
 
 module.exports = {
   name: "remove",
-  alias: ["rem","fuck","bhag","Nikal"],
-  desc: "Remove a member from group",
-  category: "Group",
-  usage: "remove @user",
-  react: "🗿",
-  start: async (
-    Miku,
-    m,
-    { text, prefix, isBotAdmin, isAdmin, mentionByTag,pushName}
-  ) => {
-    require("../../config.js");
-require("../../Core.js");
-
-module.exports = {
-  name: "remove",
   alias: ["rem"],
   desc: "Remove a member from group",
   category: "Group",
@@ -37,29 +22,6 @@ module.exports = {
         { text: `Please tag a user to *Remove* !` },
         { quoted: m }
       );
-    } else if (m.quoted) {
-      var mentionedUser = m.quoted.sender;
-    } else {
-      var mentionedUser = mentionByTag[0];
-    }
-
-    let users = (await mentionedUser) || m.msg.contextInfo.participant;
-
-    try {
-      await Miku.groupParticipantsUpdate(m.from, [users], "remove").then(
-        (res) =>
-          Miku.sendMessage(
-            m.from,
-            { text: `@${mentionedUser.split("@")[0]} has been *Removed* Successfully by *${pushName}*` },
-            { quoted: m }
-          )
-      );
-    } catch (err) {
-      Miku.sendMessage(m.from, { text: `${mess.botadmin}` }, { quoted: m });
-    }
-  },
-};
-    if (!text && !m.quoted) return m.reply(`Please tag a user to *Remove* from group!`)
     } else if (m.quoted) {
       var mentionedUser = m.quoted.sender;
     } else {
