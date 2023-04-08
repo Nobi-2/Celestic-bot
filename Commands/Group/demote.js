477b4990f47b8d5ae37627c1bd057cb3ed64f44e
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-08 13:42:14 +0530

    Update demote.js

--- Commands/Group/demote.js
+++ Commands/Group/demote.js
@@ -10,7 +10,8 @@ module.exports = {
     m,
     { text, prefix, isBotAdmin, isAdmin, mentionByTag, pushName, groupAdmin }
   ) => {
-    if (!isAdmin && !isBotAdmin) return m.reply(`Bot and *${pushName}* both must be admin in order to use this command !`);
+    if (!isAdmin) return m.reply(`*${pushName}* must be admin in order to use this command !`);
+    if (!isBotAdmin) return m.reply(`Bot must be admin in order to use this command !`);
 
     if (!text && !m.quoted) {
       return Miku.sendMessage(
