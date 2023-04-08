f1111bdadaca44243532c8e22e81b2637122ca2b
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-08 12:34:18 +0530

    Update remove.js

--- Commands/Group/remove.js
+++ Commands/Group/remove.js
@@ -16,6 +16,12 @@ module.exports = {
     if (!isAdmin && !isBotAdmin) return m.reply(`Bot and *${pushName}* both must be admin in order to use this command !`);
   
     if (!text && !m.quoted) return m.reply(`Please tag a user to *Remove* from group!`);
+    if (!text && !m.quoted) {
+      return Miku.sendMessage(
+        m.from,
+        { text: `Please tag a user to *Remove* !` },
+        { quoted: m }
+      );
     } else if (m.quoted) {
       var mentionedUser = m.quoted.sender;
     } else {
