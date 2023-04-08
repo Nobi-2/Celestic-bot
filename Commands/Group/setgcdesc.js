d9f4daa6616383d5af1149c88199033b4a6c368b
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-08 13:27:22 +0530

    Update setgcdesc.js

--- Commands/Group/setgcdesc.js
+++ Commands/Group/setgcdesc.js
@@ -10,7 +10,8 @@ module.exports = {
     m,
     { text, prefix, isBotAdmin, isAdmin, pushName, metadata, args, mime }
   ) => {
-    if (!isAdmin && !isBotAdmin) return m.reply(`Bot and *${pushName}* both must be admin in order to use this command !`);
+    if (!isAdmin) return m.reply(`*${pushName}* must be admin in order to use this command !`);
+    if (!isBotAdmin) return m.reply(`Bot must be admin in order to use this command !`);
     if (!args[0])
       return Miku.sendMessage(
         m.from,
