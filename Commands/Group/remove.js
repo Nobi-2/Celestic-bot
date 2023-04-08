1e8f2ffedea7456c2b3c5ebf3e95e66750dd254b
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-08 12:39:45 +0530

    Update remove.js

--- Commands/Group/remove.js
+++ Commands/Group/remove.js
@@ -13,8 +13,8 @@ module.exports = {
     m,
     { text, prefix, isBotAdmin, isAdmin, mentionByTag,pushName}
   ) => {
-    if (!isAdmin && !isBotAdmin) return m.reply(`Bot and *${pushName}* both must be admin in order to use this command !`);
-  
+    if (!isAdmin) return m.reply(`*${pushName}* both must be admin in order to use this command !`);
+    if (!isBotAdmin) return m.reply(`Bot must be admin in order to use this command !`);
     if (!text && !m.quoted) return m.reply(`Please tag a user to *Remove* from group!`);
     if (!text && !m.quoted) {
       return Miku.sendMessage(
