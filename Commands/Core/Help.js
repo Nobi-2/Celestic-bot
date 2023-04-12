7702888f760e260bcbae56764c313b68c22ef08d
Kingshisui00 <127178249+Kingshisui00@users.noreply.github.com>
2023-04-12 15:07:41 +0530

    Create Help.js

---
+++ Commands/Core/Help.js
@@ -0,0 +1,48 @@
+const { mku } = require('../../Database/dataschema.js');
+const { sessionSchema } = require('../../Database/index.js');
+const { runtime } = require('../../lib/myfunc.js');
+
+module.exports = {
+    name: 'help',
+    desc: 'Displays bot lists',
+    alias: ['menu','h'],
+    category: 'Core',
+    usage: 'stats',
+    react: '🔢',
+    start: async (Miku, m, { text, prefix, mentionByTag, pushName, botName, isCreator, participants, modStatus, commands, store, from }) => {
+      try {
+      const [modlist, FetchGC, totalUsers, sessionCount] = await Promise.all([
+        mku.find({ addedMods: 'true' }),
+        Miku.groupFetchAllParticipating(),
+        mku.find({}),
+        sessionSchema.countDocuments(),
+      ]);
+
+      const groups = Object.entries(FetchGC).map(entry => entry[1]);
+      const groupIds = groups.map(v => v.id);
+      const modIds = modlist.map(mod => mod.id);
+      const cmds = Array.from(commands.values()).filter(v => v.type !== 'hide').length;
+
+      const statsText = generateStatsText(totalUsers.length, 2, groupIds.length, modIds.length, cmds, sessionCount, process.uptime());
+      return Miku.sendMessage(m.from, { text: statsText }, { quoted: m });
+
+    } catch (error) {
+      console.error('Error in stats command:', error);
+      return Miku.sendMessage(m.from, { text: 'An error occurred while fetching bot statistics.' }, { quoted: m });
+    }
+  },
+};
+
+function generateStatsText(users, bots, groups, mods, commands, sessions, uptime) 
+  return `
+*━━━❰ STATISTICS ❱━━━*
+*bot details*
+1) Type help-c to get all the commands list of our js bot
+only our js bot will.provide this helplist.
+
+2)type help-g to see all commands list of our gamebot js bot can only show command but it cant perform those if u want to jse those commands use ts bot.
+
+3)type help-r to get all our rules amd follow every single rules
+
+4)type help-a to get all details of our celestic botz association read carefully to get all sources`;
+        }
