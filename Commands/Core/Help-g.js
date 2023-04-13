32e78c27133f767a678d2f6c4fb99626241eaf1c
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-14 02:21:35 +0530

    Update Help-g.js

--- Commands/Core/Help-g.js
+++ Commands/Core/Help-g.js
@@ -40,43 +40,43 @@ TYPE -HELP
 
 *➩ Characters Side*
 
-```❐ claim, gchara, gallery, schara```
+   ❐ claim, gchara, gallery, schara
 
 *➩ Economy Side*
 
-```❐ bank, daily, deposit, gamble, give, rob, slot, wallet, withdraw```
+   ❐ bank, daily, deposit, gamble, give, rob, slot, wallet, withdraw
 
 *➩ Fun Side*
 
-```❐ friendship, reaction, ship, simp, triggered, fact```
+   ❐ friendship, reaction, ship, simp, triggered, fact
 
 *➩ Games Side*
 
-```❐ answer, forfeit-quiz, quiz```
+   ❐ answer, forfeit-quiz, quiz
 
 *➩ General Side*
 
-```❐ help, hi, info, mods, profile, rank, repo, set-bio```
+   ❐ help, hi, info, mods, profile, rank, repo, set-bio
 
 *➩ Media Side*
 
-```❐ lyrics, play, spotify, yta, yts, ytv```
+   ❐ lyrics, play, spotify, yta, yts, ytv
 
 *➩ Moderation Side*
 
-```❐ close, delete, demote, open, ping, promote, purge, remove, set```
+   ❐ close, delete, demote, open, ping, promote, purge, remove, set
 
 *➩ Weeb Side*
 
-```❐ manga, anime, character, divorce, haigusha, hg, kitsune, loli, marry, neko, waifu```
+   ❐ manga, anime, character, divorce, haigusha, hg, kitsune, loli, marry, neko, waifu
 
 *➩ Pokemon Side*
 
-```❐ catch, pokedex, party, pc, pokemon, swap, t2party, t2pc, trade, trade-confirm, trade-delete```
+   ❐ catch, pokedex, party, pc, pokemon, swap, t2party, t2pc, trade, trade-confirm, trade-delete
 
 *➩ Utils Side*
 
-```❐ define, github, prettier, quotly, react, retrieve, steal, sticker, upload, urbandictionary```
+   ❐ define, github, prettier, quotly, react, retrieve, steal, sticker, upload, urbandictionary
 
 📕 *Note: we are doing iur best to bring update*
 `;
