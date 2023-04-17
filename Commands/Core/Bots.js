357c6c2e461797dfe96198de7682d6b053d1464d
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-17 16:05:02 +0530

    Update Bots.js

--- Commands/Core/Bots.js
+++ Commands/Core/Bots.js
@@ -25,4 +25,20 @@ DESC = HERE U CAN ENJOY OUR FEATURES
 
 ━━━❰ PUBLIC GROUPS ❱━━━
 LINK = 
-DESC = YOU CAN ENJOY HERE THE THIRD BIT AND OUR PUBLIC NOT HELPS DURING DEPLOY.
+DESC = YOU CAN ENJOY HERE THE THIRD BOT AND OUR PUBLIC NOT HELPS DURING DEPLOY.\n\n
+ᴄᴇʟᴇsᴛɪᴄ ʙᴏᴛᴢ ᴄᴏɴᴛᴀᴄᴛs
+ ᴛᴇʟᴇɢʀᴀᴍ = https://t.me/+zJpompWc6SE1YTI9
+ ᴅɪsᴄᴏʀᴅ = https://discord.gg/ETEZrh6a6k
+ ᴄᴀsɪɴᴏ = https://chat.whatsapp.com/DrY5MBaiDRS9BAcpCpJQCv
+ ᴡʜᴀᴛsᴀᴀᴘ = https://chat.whatsapp.com/EodcJDysqP0LInOzcaPRC1
+ ᴡᴇʙsɪᴛᴇ =   https://celestic-website-2.celestic1.repl.co
+ ʏᴏᴜᴛᴜʙᴇ =  https://youtube.com/@CELESTIC-bottoinc
+ ᴍᴀɪɴ ᴡᴇʙ = https://celesticbotz.000webhostapp.com/
+ ɢᴍᴀɪʟ = Celestic.bot.inc@gmail.com
+ 
+ᴛᴏ ᴄᴏɴᴛᴀᴄᴛ ᴍᴏᴅs ᴛʏᴘᴇ ${prefix}ᴅᴇᴠs
+ᴄᴇʟᴇsᴛɪᴄ ʙᴏᴛᴢ ɪɴᴄ©2023 ʙʏ ᴛᴇᴀᴍ ᴄᴇʟᴇsᴛɪᴄ
+ᴄᴇʟᴇsᴛɪᴄ ʙᴏᴛᴢ ɪɴᴄ`;
+        await Miku.sendMessage(m.from, {video: { url: botVideo }, gifPlayback: true, caption: textHelpMenu,}, { quoted: m });
+     }
+}
