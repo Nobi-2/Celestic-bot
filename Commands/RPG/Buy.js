496a9718baf48336fb3ac631423158bde04e9205
Lucky kumar <119100799+lucky01u@users.noreply.github.com>
2023-04-17 14:13:02 +0530

    Update Buy.js

--- Commands/RPG/Buy.js
+++ Commands/RPG/Buy.js
@@ -38,57 +38,56 @@ module.exports = {
             user.inventory.stonepickaxe += 1;
             await user.save();
             Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a stone pickaxe!`}, { quoted: m });
-}
-else if(item === "diamondpickaxe") {
-    if(user.inventory.silverApple < 3) {
-        return Miku.sendMessage(m.from, { text:` 😕 You don't have silver Apples to purchase a diamond pickaxe .` }, { quoted: m });
-    }
-    user.inventory.diamondpickaxe += 1;
-    user.inventory.silverApple -= 3;
-    await user.save();
-    Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a diamond pickaxe!`}, { quoted: m });
-}
-else if(item === "legendpickaxe") {
-    if(user.inventory.goldenApple < 2) {
-        return Miku.sendMessage(m.from, { text:` 😕 You don't have golden Apples to purchase a diamond pickaxe .` }, { quoted: m });
-    }
-    user.inventory.heroicpickaxe += 1;
-    user.inventory.goldenApple -= 2;
-    await user.save();
-    Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a heroic pickaxe!`}, { quoted: m });
-}
-else if(item === "godpickaxe") {
-    if(user.inventory.goldenApple < 10) {
-        return Miku.sendMessage(m.from, { text:` 😕 You don't have diamond Apples to purchase a diamond pickaxe .` }, { quoted: m });
-    }
-    user.inventory.godpickaxe += 1;
-    user.inventory.goldenApple -= 10;
-    await user.save();
-    Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a diamond pickaxe!`}, { quoted: m });
-}
-
-else if(item === "ironpickaxe") {
-if(balance.wallet < 1000000) {
-return Miku.sendMessage(m.from, { text: `😕 You don't have enough coins to purchase an iron pickaxe. `}, { quoted: m });
-}
-let deduct = await eco.deduct(m.sender, cara, 1000000);
-user.inventory.ironpickaxe += 1;
-await user.save();
-Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased an iron pickaxe!`}, { quoted: m });
-}
-else if(item === "platinumpickaxe") {
-if(balance.wallet < 5000000) {
-return Miku.sendMessage(m.from, { text: `😕 You don't have enough coins to purchase a platinum pickaxe.` }, { quoted: m });
-}
-let deduct = await eco.deduct(m.sender, cara, 5000000);
-user.inventory.platinumpickaxe += 1;
-await user.save();
-Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a platinum pickaxe!`}, { quoted: m });
-}
-else {
-return Miku.sendMessage(m.from, { text: `😕 Invalid item. Please use ${prefix}buy /item to purchase an item.` }, { quoted: m });
-}
-}
+        }
+        else if(item === "diamondpickaxe") {
+            if(user.inventory.silverApple < 3) {
+                return Miku.sendMessage(m.from, { text:` 😕 You don't have silver Apples to purchase a diamond pickaxe .` }, { quoted: m });
+            }
+            user.inventory.diamondpickaxe += 1;
+            user.inventory.silverApple -= 3;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a diamond pickaxe!`}, { quoted: m });
+        }
+        else if(item === "legendpickaxe") {
+            if(user.inventory.goldenApple < 2) {
+                return Miku.sendMessage(m.from, { text:` 😕 You don't have golden Apples to purchase a diamond pickaxe .` }, { quoted: m });
+            }
+            user.inventory.heroicpickaxe += 1;
+            user.inventory.goldenApple -= 2;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a heroic pickaxe!`}, { quoted: m });
+        }
+        else if(item === "godpickaxe") {
+            if(user.inventory.goldenApple < 10) {
+                return Miku.sendMessage(m.from, { text:` 😕 You don't have diamond Apples to purchase a diamond pickaxe .` }, { quoted: m });
+            }
+            user.inventory.godpickaxe += 1;
+            user.inventory.goldenApple -= 10;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a diamond pickaxe!`}, { quoted: m });
+       }
+       else if(item === "ironpickaxe") {
+           if(balance.wallet < 1000000) {
+               return Miku.sendMessage(m.from, { text: `😕 You don't have enough coins to purchase an iron pickaxe. `}, { quoted: m });
+            }
+            let deduct = await eco.deduct(m.sender, cara, 1000000);
+            user.inventory.ironpickaxe += 1;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased an iron pickaxe!`}, { quoted: m });
+       }
+       else if(item === "platinumpickaxe") {
+           if(balance.wallet < 5000000) {
+               return Miku.sendMessage(m.from, { text: `😕 You don't have enough coins to purchase a platinum pickaxe.` }, { quoted: m });
+            }
+            let deduct = await eco.deduct(m.sender, cara, 5000000);
+            user.inventory.platinumpickaxe += 1;
+            await user.save();
+            Miku.sendMessage(m.from, { text: `[ *💰PURCHASE RESULT💰* ]\n\n Successfully purchased a platinum pickaxe!`}, { quoted: m });
+       }
+       else {
+       return Miku.sendMessage(m.from, { text: `😕 Invalid item. Please use ${prefix}buy /item to purchase an item.` }, { quoted: m });
+       }
+  }
 };
 
               
